4834302f843b01ec599543655a98ee5f1153eb5e
zhao <zztemp001@gmail.com>
2013-05-29 13:30:24 +0800

    update

--- yaayaa/fabfile_yaayaa.py
+++ yaayaa/fabfile_yaayaa.py
@@ -9,18 +9,22 @@ def update_project():
     # 更新服务器代码并收集静态文件
     with cd('~/sites/yaayaa'):
         run('git pull origin master')
-        run('source ~/envs/yaayaa.com/bin/activate')
-        run('python manage.py collectstatic --noinput')
+        with prefix('source ~/envs/yaayaa.com/bin/activate'):
+            run('python manage.py collectstatic --noinput')
 
 def restart_server():
     # 重启 gunicorn
-    run('killall -9 gunicorn_django')
-    run('gunicorn_django --daemon ./yaayaa/settings_yaayaa.py')
+    try:
+        run('killall -9 gunicorn_django')
+    except:
+        pass
+
+    run('gunicorn_django -D ./yaayaa/settings_yaayaa.py')
 
     # 重启 nginx
     sudo('nginx -s reload')
 
 def deploy():
     run('uname -s')
-    #update_project()
-    #restart_server()
+    update_project()
+    restart_server()
